b22d191d3ff4569d3274ef246d781b3e852f8297
Elias Reich <116839305+dancingacidpanda@users.noreply.github.com>
2025-05-05 16:23:06 +0200

    Update setup.py
    updated authors

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='cycle_detection',
     version='1.0',
     description='Topology-driven identification of repetitions in multi-variate time series',
-    author='Elias Reich, Simon Schindler',
+    author='Elias Reich, Stefan Huber, Simon Schindler',
     author_email='eliassteffen.reich@fh-salzburg.ac.at',
     url='https://github.com/JRC-ISIA/paper-2025-idsc-topology-driven-identification-of-repetitions-in-multi-variate-time-series/',
     packages=[
